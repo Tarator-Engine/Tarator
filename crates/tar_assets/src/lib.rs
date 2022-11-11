b86edfcab2b9f81b7529068b6a2706159591be22
slackers <47892117+slackers123@users.noreply.github.com>
2022-11-11 16:54:47 +0100

    updated asset loader models to spec

--- crates/tar_assets/src/lib.rs
+++ crates/tar_assets/src/lib.rs
@@ -3,7 +3,6 @@ use std::path::Path;
 use std::{io::{BufReader, Cursor}};
 
 use image::DynamicImage;
-use tobj::LoadError;
 
 #[macro_use]
 extern crate thiserror;
@@ -16,7 +15,35 @@ extern crate serde;
 
 mod model;
 
-use model::*;
+trait Vec2Slice<T> {
+    fn as_slice(self) -> [T; 2];
+}
+
+impl<T> Vec2Slice<T> for cgmath::Vector2<T> {
+    fn as_slice(self) -> [T; 2] {
+        [self.x, self.y]
+    }
+}
+
+trait Vec3Slice<T> {
+    fn as_slice(self) -> [T; 3];
+}
+
+impl<T> Vec3Slice<T> for cgmath::Vector3<T> {
+    fn as_slice(self) -> [T; 3] {
+        [self.x, self.y, self.z]
+    }
+}
+
+trait Vec4Slice<T> {
+    fn as_slice(self) -> [T; 4];
+}
+
+impl<T> Vec4Slice<T> for cgmath::Vector4<T> {
+    fn as_slice(self) -> [T; 4] {
+        [self.x, self.y, self.z, self.w]
+    }
+}
 
 #[derive(Error, Debug)]
 pub enum ImportError {
@@ -29,11 +56,6 @@ pub enum ImportError {
     Image {
         #[from]
         source: image::ImageError
-    },
-    #[error("obj Error")]
-    Tobj {
-        #[from]
-        source: tobj::LoadError,
     },
     #[error("gltf Error")]
     Gltf {
@@ -87,205 +109,6 @@ pub enum ExportError {
     MissingPath,
 }
 
-pub struct AssetRef {
-    name: String,
-    fs_id: u32,
+fn get_asset_cache() -> Result<assets_manager::AssetCache, std::io::Error> {
+    assets_manager::AssetCache::new("assets")
 }
-
-
-pub async fn load_asset_from_path(file_path: &str) -> Result<AssetRef, ImportError> {
-
-    let path = Path::new(file_path);
-
-    match path.extension().ok_or(ImportError::NoFileExtension)?.to_str().ok_or(ImportError::NoFileExtension)? {
-
-        "gltf" | "glb" => {
-            import_gltf(path)
-        }
-
-        // "obj" => {
-        //     import_obj(path).await
-        // }
-
-        "mp3" | "wav" | "ogg" => {
-            todo!("audio loading")
-        }
-
-        f => {return Err(ImportError::UnsupportedFileExtension(f.to_owned()))}
-    }
-}
-
-pub fn import_gltf(path: &Path) -> Result<AssetRef, ImportError> {
-    todo!()
-}
-
-pub fn import_image(path: &Path) -> Result<DynamicImage, ImportError> {
-    image::open(path).map_err(|e| ImportError::Image { source: e })
-}
-
-pub async fn load_binary(path: &Path) -> Result<Vec<u8>, ImportError> {
-    cfg_if! {
-        if #[cfg(target_arch = "wasm32")] {
-            let url = format_url(file_name);
-            let data = reqwest::get(url)
-                .await?
-                .bytes()
-                .await?
-                .to_vec();
-        } else {
-            println!("loading binary {:?}", path);
-            let data = std::fs::read(path)?;
-        }
-    }
-
-    Ok(data)
-}
-
-pub async fn load_string(path: &Path) -> Result<String, ImportError> {
-    cfg_if! {
-        if #[cfg(target_arch = "wasm32")] {
-            let url = format_url(file_name);
-            let txt = reqwest::get(url)
-                .await?
-                .text()
-                .await?;
-        } else {
-            println!("loading string: {:?}", path);
-            let txt = std::fs::read_to_string(path).map_err(|e| e)?;
-        }
-    }
-
-    Ok(txt)
-}
-
-// pub async fn import_obj(
-//     path: &Path,
-// ) -> Result<AssetRef, ImportError> {
-//     let obj_text = load_string(path).await?;
-//     let obj_cursor = Cursor::new(obj_text);
-//     let mut obj_reader = BufReader::new(obj_cursor);
-
-//     let parent_dir = path.ancestors().nth(1).ok_or(ImportError::MissingParentDirectory)?;
-
-//     let (models, obj_materials) = tobj::load_obj_buf_async(
-//         &mut obj_reader,
-//         &tobj::LoadOptions {
-//             triangulate: true,
-//             single_index: true,
-//             ..Default::default()
-//         },
-//         |p| async move {
-//             let mat_text = load_string(&path.join(p.to_owned())).await.map_err(|e| LoadError::OpenFileFailed)?;
-//             tobj::load_mtl_buf(&mut BufReader::new(Cursor::new(mat_text)))
-//         },
-//     )
-//     .await?;
-    
-//     let mut images = HashMap::new();
-//     if obj_materials.is_ok() {
-//         for m in obj_materials? {
-//             let diffuse_texture = image::open(path.join(m.diffuse_texture)).map_err(|e| ExportError::Image { source: e })?;
-//             let normal_texture = image::open(path.join(m.normal_texture)).map_err(|e| ExportError::Image { source: e })?;
-//             images.insert(m.diffuse_texture, diffuse_texture);
-//             images.insert(m.normal_texture, normal_texture);
-
-//         }
-//     }
-
-//     let meshes = models
-//     .into_iter()
-//     .map(|m| {
-//         let mut vertices = (0..m.mesh.positions.len() / 3)
-//             .map(|i| Vertex {
-//                 position: [
-//                     m.mesh.positions[i * 3],
-//                     m.mesh.positions[i * 3 + 1],
-//                     m.mesh.positions[i * 3 + 2],
-//                 ],
-//                 tex_coords: [m.mesh.texcoords[i * 2], m.mesh.texcoords[i * 2 + 1]],
-//                 normal: [
-//                     m.mesh.normals[i * 3],
-//                     m.mesh.normals[i * 3 + 1],
-//                     m.mesh.normals[i * 3 + 2],
-//                 ],
-//                 // We'll calculate these later
-//                 tangent: [0.0; 4],
-//             })
-//             .collect::<Vec<_>>();
-
-//         let indices = &m.mesh.indices;
-//         let mut triangles_included = vec![0; vertices.len()];
-
-//         // Calculate tangents and bitangets. We're going to
-//         // use the triangles, so we need to loop through the
-//         // indices in chunks of 3
-//         for c in indices.chunks(3) {
-//             let v0 = vertices[c[0] as usize];
-//             let v1 = vertices[c[1] as usize];
-//             let v2 = vertices[c[2] as usize];
-
-//             let pos0: cgmath::Vector3<_> = v0.position.into();
-//             let pos1: cgmath::Vector3<_> = v1.position.into();
-//             let pos2: cgmath::Vector3<_> = v2.position.into();
-
-//             let uv0: cgmath::Vector2<_> = v0.tex_coords.into();
-//             let uv1: cgmath::Vector2<_> = v1.tex_coords.into();
-//             let uv2: cgmath::Vector2<_> = v2.tex_coords.into();
-
-//             // Calculate the edges of the triangle
-//             let delta_pos1 = pos1 - pos0;
-//             let delta_pos2 = pos2 - pos0;
-
-//             // This will give us a direction to calculate the
-//             // tangent and bitangent
-//             let delta_uv1 = uv1 - uv0;
-//             let delta_uv2 = uv2 - uv0;
-
-//             // Solving the following system of equations will
-//             // give us the tangent and bitangent.
-//             //     delta_pos1 = delta_uv1.x * T + delta_u.y * B
-//             //     delta_pos2 = delta_uv2.x * T + delta_uv2.y * B
-//             // Luckily, the place I found this equation provided
-//             // the solution!
-//             let r = 1.0 / (delta_uv1.x * delta_uv2.y - delta_uv1.y * delta_uv2.x);
-//             let tangent = (delta_pos1 * delta_uv2.y - delta_pos2 * delta_uv1.y);
-
-//             let tangent = cgmath::Vector4::new(tangent.x, tangent.y, tangent.z, r); // TODO: -r?
-
-//             // We'll use the same tangent/bitangent for each vertex in the triangle
-//             vertices[c[0] as usize].tangent =
-//                 (tangent + cgmath::Vector4::from(vertices[c[0] as usize].tangent)).into();
-//             vertices[c[1] as usize].tangent =
-//                 (tangent + cgmath::Vector4::from(vertices[c[1] as usize].tangent)).into();
-//             vertices[c[2] as usize].tangent =
-//                 (tangent + cgmath::Vector4::from(vertices[c[2] as usize].tangent)).into();
-
-//             // Used to average the tangents/bitangents
-//             triangles_included[c[0] as usize] += 1;
-//             triangles_included[c[1] as usize] += 1;
-//             triangles_included[c[2] as usize] += 1;
-//         }
-
-//         // Average the tangents/bitangents
-//         for (i, n) in triangles_included.into_iter().enumerate() {
-//             let denom = 1.0 / n as f32;
-//             let mut v = &mut vertices[i];
-//             v.tangent = (cgmath::Vector4::from(v.tangent) * denom).into();
-//         }
-
-//         Model {
-
-//         }
-//     })
-//     .collect::<Vec<_>>();
-
-
-//     let model = crate::model::Model {
-//         // unwrap_justified as there would have been an error earlyer
-//         name: path.to_str().unwrap().to_owned(),
-//         vertices: meshes[0],
-
-//     };
-
-//     save_model(model, images, None).await.map_err(|e| ImportError::ExportError { source: e })
-// }
--- crates/tar_assets/src/model.rs
+++ crates/tar_assets/src/model.rs
@@ -1,225 +1,132 @@
-use std::{collections::HashMap, hash};
-use std::path::Path;
-
-use gltf::json::extensions::material;
-use image::{
-    RgbImage,
-    RgbaImage,
-    GrayImage,
-    Rgb, DynamicImage
-};
-
 use serde::{Serialize, Deserialize};
 
-use crate::{ExportError, AssetRef, ImportError};
+use uuid::Uuid;
 
-type ProjPath = String;
-
-
-#[derive(Serialize, Deserialize, Clone)]
 /// Model to be saved and loaded to and from disk
-pub struct Model {
-    pub name: String,
-    pub vertices: Vec<Vertex>,
-    pub indices: Vec<usize>,
-    pub material: Material,
+pub struct RawModel {
+    pub id: Uuid,
+    pub meshes: Vec<RawMesh>,
+    pub materials: Vec<RawMaterial>,
+    pub instances: wgpu::Buffer,
+    pub instance_num: u32,
 }
 
-#[derive(Serialize, Deserialize, Clone)]
 /// Contains a position, normal and texture coordinates vectors.
-pub struct Vertex {
-    /// Position
+pub struct RawMesh {
+    pub vertecies: Option<wgpu::Buffer>,
+    pub indecies: Option<wgpu::Buffer>,
+    pub num_indecies: u32,
+    pub material: u32,
+}
+
+/// Contains material properties of models.
+pub struct RawMaterial {
+    /// diffuse texture of a normal: this is also sometimes called base color
+    pub diffuse_tex: Option<RawTexture>,
+
+    /// normal texture of a material.
+    pub normal_tex: Option<RawTexture>,
+
+    /// metallicness texture of a material.
+    pub metallic_tex: Option<RawTexture>,
+
+    /// roughness texture of a material
+    pub roughness_tex: Option<RawTexture>,
+
+    /// Occlusion Texture of a material
+    pub occlusion_tex: Option<RawTexture>,
+
+    /// The emissive color of the material.
+    pub emissive_tex: Option<RawTexture>,
+
+    /// the info buffer contains extra informatino aobout the
+    /// material and fallback values in case some textures
+    /// aren't provided
+    pub imfo_buffer: wgpu::Buffer,
+}
+
+pub struct RawTexture {
+    pub texture: wgpu::Texture,
+    pub view: wgpu::TextureView,
+    pub sampler: wgpu::Sampler,
+}
+
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct StoreModel {
+    pub id: Uuid,
+    pub meshes: Vec<StoreMesh>,
+    pub materials: Vec<StoreMaterial>,
+    pub instances: Vec<StoreInstance>,
+    pub instance_num: u32,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct StoreMesh {
+    pub vertecies: Vec<StoreVertex>,
+    pub indecies: Vec<u32>,
+    pub num_indecies: u32,
+    pub material: u32,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct StoreVertex {
     pub position: [f32; 3],
-
-    /// Normalized normal
     pub normal: [f32; 3],
-
-    /// Tangent normal 
-    /// The 'w' component indicates the direction of the vector
-    /// 
-    /// Note: to claculate the bitangent do: `cross(normal, tangent) * tangent.w`
     pub tangent: [f32; 4],
-
-    /// Texture coordinates (UV)
     pub tex_coords: [f32; 2],
 }
 
-#[derive(Serialize, Deserialize, Clone)]
-/// Contains material properties of models.
-pub struct Material {
-    /// Parameter values that define the metallic-roughness material model from
-    /// Physically-Based Rendering (PBR) methodology.
-    pub pbr: PbrMaterial,
+#[derive(Debug, Serialize, Deserialize)]
+pub struct StoreMaterial {
+    /// diffuse texture of a normal: this is also sometimes called base color
+    pub diffuse_tex: std::path::PathBuf,
 
-    /// Defines the normal texture of a material.
-    pub normal: Option<NormalMap>,
+    /// normal texture of a material.
+    pub normal_tex: std::path::PathBuf,
 
-    /// Defines the occlusion texture of a material.
-    pub occlusion: Option<Occlusion>,
+    /// metallicness texture of a material.
+    pub metallic_tex: std::path::PathBuf,
+
+    /// roughness texture of a material
+    pub roughness_tex: std::path::PathBuf,
+
+    /// Occlusion Texture of a material
+    pub occlusion_tex: std::path::PathBuf,
 
     /// The emissive color of the material.
-    pub emissive: Emissive,
-}
+    pub emissive_tex: std::path::PathBuf,
 
-#[derive(Serialize, Deserialize, Clone)]
-/// A set of parameter values that are used to define the metallic-roughness
-/// material model from Physically-Based Rendering (PBR) methodology.
-pub struct PbrMaterial {
     /// The `base_color_factor` contains scaling factors for the red, green,
     /// blue and alpha component of the color. If no texture is used, these
     /// values will define the color of the whole object in **RGB** color space.
     pub base_color_factor: [f32; 4],
-
-    /// The `base_color_texture` is the main texture that will be applied to the
-    /// object.
-    ///
-    /// The texture contains RGB(A) components in **sRGB** color space.
-    pub base_color_texture: Option<RgbaImageRef>,
-
-    /// Contains the metalness value
-    pub metallic_texture: Option<GrayImageRef>,
 
     /// `metallic_factor` is multiply to the `metallic_texture` value. If no
     /// texture is given, then the factor define the metalness for the whole
     /// object.
     pub metallic_factor: f32,
 
-    /// Contains the roughness value
-    pub roughness_texture: Option<GrayImageRef>,
-
     /// `roughness_factor` is multiply to the `roughness_texture` value. If no
     /// texture is given, then the factor define the roughness for the whole
     /// object.
     pub roughness_factor: f32,
-}
-
-#[derive(Serialize, Deserialize, Clone)]
-/// Defines the normal texture of a material.
-pub struct NormalMap {
-    /// A tangent space normal map.
-    /// The texture contains RGB components in linear space. Each texel
-    /// represents the XYZ components of a normal vector in tangent space.
-    ///
-    /// * Red [0 to 255] maps to X [-1 to 1].
-    /// * Green [0 to 255] maps to Y [-1 to 1].
-    /// * Blue [128 to 255] maps to Z [1/255 to 1].
-    ///
-    /// The normal vectors use OpenGL conventions where +X is right, +Y is up,
-    /// and +Z points toward the viewer.
-    pub texture: RgbImageRef,
-
+    
     /// The `normal_factor` is the normal strength to be applied to the
     /// texture value.
-    pub factor: f32,
-}
-
-#[derive(Serialize, Deserialize, Clone)]
-/// Defines the occlusion texture of a material.
-pub struct Occlusion {
-    /// The `occlusion_texture` refers to a texture that defines areas of the
-    /// surface that are occluded from light, and thus rendered darker.
-    pub texture: GrayImageRef,
+    pub normal_factor: f32,
 
     /// The `occlusion_factor` is the occlusion strength to be applied to the
     /// texture value.
-    pub factor: f32,
-}
-
-#[derive(Serialize, Deserialize, Clone)]
-/// The emissive color of the material.
-pub struct Emissive {
-    /// The `emissive_texture` refers to a texture that may be used to illuminate parts of the
-    /// model surface: It defines the color of the light that is emitted from the surface
-    pub texture: Option<RgbImageRef>,
+    pub occlusion_factor: f32,
 
     /// The `emissive_factor` contains scaling factors for the red, green and
     /// blue components of this texture.
-    pub factor: [f32; 3],
+    pub emissive_factor: [f32; 3],
 }
 
-#[derive(Serialize, Deserialize, Clone)]
-/// A reference to an Rgb image
-pub struct ImageRef {
-    /// Name of an image
-    pub name: String,
-    /// Relative path to an image
-    pub location: Option<ProjPath>,
-}
-
-type RgbImageRef = ImageRef;
-type GrayImageRef = ImageRef;
-type RgbaImageRef = ImageRef;
-
-
-/// saves a model and its images to the the path specified in target (relative to base directory) or the base directory
-/// 
-pub async fn save_model(model: Model, images: HashMap<String, DynamicImage>, target: Option<ProjPath>)-> Result<AssetRef, ExportError> {
-    let buf = rmp_serde::to_vec(&model)?;
-    let path = if let Some(p) = target {
-        get_abs_path(p)?
-    }
-    else {
-        get_abs_path(model.name)?
-    };
-
-    std::fs::write(path.clone(), buf)?;
-
-    for image in images {
-        image.1.save(get_abs_path(image.0)?)?;
-    }
-
-    // unwrap here is justified as there would have been an error earlier if the path was invalid
-    Ok(AssetRef { name: path, fs_id: 0 })
-}
-
-/// gets the absolute path from a relative path in the project directory
-pub fn get_abs_path(local_path: ProjPath) -> Result<String, ExportError> {
-    // unwrap_justified would have errored earlier if there was an error
-    Ok(Path::new(&std::env::current_dir()?).join(local_path).to_str().unwrap().to_owned())
-}
-
-/// loads a model from the project path
-pub fn load_model(path: ProjPath) -> Result<(Model, HashMap<String, DynamicImage>), ExportError> {
-    let model = rmp_serde::from_slice(std::fs::read(path)?.as_slice())?;
-
-    let images = load_images_from_model(&model)?;
-
-    Ok((model, images))
-}
-
-/// loads the images form a model
-pub fn load_images_from_model(model: &Model) -> Result<HashMap<String, DynamicImage>, ExportError> {
-    let mut images = HashMap::new();
-
-    let material = &model.material;
-    let pbr = &material.pbr;
-    if let Some(bt) = &pbr.base_color_texture {
-        images.insert(bt.name.clone(), load_local_image(bt.location.clone())?);
-    }
-    if let Some(i) = &pbr.metallic_texture {
-        images.insert(i.name.clone(), load_local_image(i.location.clone())?);
-    }
-    if let Some(i) = &pbr.roughness_texture {
-        images.insert(i.name.clone(), load_local_image(i.location.clone())?);
-    }
-
-    if let Some(normal) = &material.normal {
-        let i = &normal.texture;
-        images.insert(i.name.clone(), load_local_image(i.location.clone())?);
-    }
-
-    let emissive = &material.emissive;
-    if let Some(i) = &emissive.texture {
-        images.insert(i.name.clone(), load_local_image(i.location.clone())?);
-    }
-
-
-    Ok(images)
-}
-
-/// loads a local image
-pub fn load_local_image(local_path: Option<String>) -> Result<DynamicImage, ExportError> {
-    let path = get_abs_path(local_path.ok_or(ExportError::MissingPath)?)?;
-
-    image::open(path).map_err(|e| ExportError::Image { source: e })
+#[derive(Debug, Serialize, Deserialize)]
+pub struct StoreInstance {
+    pub position: [f32; 3],
+    pub rotation: [f32; 4],
 }
