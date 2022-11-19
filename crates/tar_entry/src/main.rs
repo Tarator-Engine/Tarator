507f1973dedd83d92f17b810209c8c7ff3f9a4cc
slackers123 <severin.gebesmair@gmx.at>
2022-11-19 09:22:07 +0100

    small changes

--- crates/tar_entry/src/main.rs
+++ crates/tar_entry/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
     pollster::block_on(tar_assets::reset_cache()).unwrap();
-    let _id = pollster::block_on(tar_assets::import_gltf(std::path::PathBuf::from("C:/Users/slackers/Desktop/BarramundiFish.glb"), None)).unwrap();
+    let _id = pollster::block_on(tar_assets::import_gltf(std::path::PathBuf::from("/home/slackers/Documents/gltf-assets/helmet/FlightHelmet.gltf"), None)).unwrap();
 
 }
