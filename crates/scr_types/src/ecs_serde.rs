80c146f63e118e4c50962604a2aa2a3e0d800e35
Vlajd <boriskostadinov2005@gmail.com>
2023-05-16 17:39:30 +0200

    Fixed tab-space

--- crates/scr_types/src/ecs_serde.rs
+++ crates/scr_types/src/ecs_serde.rs
@@ -407,43 +407,43 @@ mod tests {
         const NAME: &'static str = "test::Bar";
     }
 
-    #[test]
-    fn serdeialize() {
-        let id = uuid::Uuid::new_v4();
-        let serialized = {
-            let mut world = World::new();
-            world.component_add_callback::<SerializeCallback, Transform>();
-            world.component_add_callback::<SerializeCallback, Foo>();
-            world.component_add_callback::<SerializeCallback, Bar>();
+#[test]
+fn serdeialize() {
+    let id = uuid::Uuid::new_v4();
+    let serialized = {
+        let mut world = World::new();
+        world.component_add_callback::<SerializeCallback, Transform>();
+        world.component_add_callback::<SerializeCallback, Foo>();
+        world.component_add_callback::<SerializeCallback, Bar>();
 
-            let entity = world.entity_create();
-            let data = (
-                Info { id, name: "GigachadEntity".into() },
-                Transform::default(),
-                Foo { foo1: 50, foo2: "We are baba!".into() },
-                Bar(vec![50, 6665, 13407, 324])
-            );
-            world.entity_set(entity, data);
+        let entity = world.entity_create();
+        let data = (
+            Info { id, name: "GigachadEntity".into() },
+            Transform::default(),
+            Foo { foo1: 50, foo2: "We are baba!".into() },
+            Bar(vec![50, 6665, 13407, 324])
+        );
+        world.entity_set(entity, data);
 
-            serde_json::to_string_pretty(&SerWorld::new(&world, uuid::Uuid::new_v4())).unwrap()
-        };
+        serde_json::to_string_pretty(&SerWorld::new(&world, uuid::Uuid::new_v4())).unwrap()
+    };
 
-        let deworld = DeWorldBuilder::new()
-            .constructor::<Transform>()
-            .constructor::<Foo>()
-            .constructor::<Bar>()
-            .build(&mut JsonDeserializer::from_str(&serialized)).unwrap();
+    let deworld = DeWorldBuilder::new()
+        .constructor::<Transform>()
+        .constructor::<Foo>()
+        .constructor::<Bar>()
+        .build(&mut JsonDeserializer::from_str(&serialized)).unwrap();
 
-        deworld.world
-            .component_query::<(Info, Transform, Foo, Bar)>()
-            .for_each(|(info, t, foo, bar)| {
-                assert!(info.name == "GigachadEntity");
-                assert!(info.id == id);
-                assert!(t == &Transform::default());
-                assert!(foo.foo1 == 50);
-                assert!(foo.foo2 == "We are baba!");
-                assert!(bar.0 == vec![50, 6665, 13407, 324]);
-            });
+    deworld.world
+        .component_query::<(Info, Transform, Foo, Bar)>()
+        .for_each(|(info, t, foo, bar)| {
+            assert!(info.name == "GigachadEntity");
+            assert!(info.id == id);
+            assert!(t == &Transform::default());
+            assert!(foo.foo1 == 50);
+            assert!(foo.foo2 == "We are baba!");
+            assert!(bar.0 == vec![50, 6665, 13407, 324]);
+        });
     }
 }
 
